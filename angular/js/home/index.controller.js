a72e07bbab37c2fcf7ba51601783950eda0f9183
Van Phan <VanPhan514@gmail.com>
2016-08-22 16:19:51 -0400

    Added map search

--- angular/js/home/index.controller.js
+++ angular/js/home/index.controller.js
@@ -3,45 +3,73 @@
 (function() {
 	angular
 	.module("home")
+  .config(['uiGmapGoogleMapApiProvider', function (GoogleMapApi) {
+    GoogleMapApi.configure({
+      key: 'AIzaSyDNzHXnxO7i2lHvIufJw9jqta_MWpHyjTg',
+      v: '3.25',
+      libraries: 'places'
+    });
+  }])
 	.controller("HomeIndexController", [
 		"HomeFactory",
 		'$scope',
+    'uiGmapGoogleMapApi',
 		HomeIndexControllerFunction
 	])
 
-	function HomeIndexControllerFunction(HomeFactory, $scope) {
+	function HomeIndexControllerFunction(HomeFactory, $scope, GoogleMapApi) {
 		$scope.epics = HomeFactory.query();
 		$scope.map = { center: { latitude: 40.7128, longitude: -74.0059 }, zoom: 8 };
-		$scope.marker = {
-      id: 0,
-      coords: {
-        latitude: 40.7128,
-        longitude: -74.0059
-      },
-      options: { draggable: true },
+    $scope.searchbox = { 
+      template:'searchbox.tpl.html', 
       events: {
-        dragend: function (marker, eventName, args) {
-          $log.log('marker dragend');
-          var lat = marker.getPosition().lat();
-          var lon = marker.getPosition().lng();
-          $log.log(lat);
-          $log.log(lon);
-
-          $scope.marker.options = {
-            draggable: true,
-            labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
-            labelAnchor: "100 0",
-            labelClass: "marker-labels"
-          };
+        places_changed: function (searchBox) {
+          console.log(searchBox)
+          console.log(searchBox.gm_accessors_.places.Qc.formattedPrediction)
+          console.log("Long " + searchBox.gm_accessors_.places.Qc.searchBoxPlaces[0].geometry
+.viewport.b.f)
+          console.log("Lat " + searchBox.gm_accessors_.places.Qc.searchBoxPlaces[0].geometry
+.viewport.f.b)
+          console.log(searchBox.gm_accessors_.places.Qc.searchBoxPlaces[0].url)
+          // $scope.markerList.push()
         }
       }
     };
+    $scope.options = { scrollwheel: false };
+    $scope.markerList = [
+      {
+        id: 0,
+        coords: {
+          latitude: 40.7128,
+          longitude: -74.0059
+        },
+        title: "<a href='/#/stories'>me</a>"
+      },
+      {
+        id: 1,
+        coords: {
+          latitude: 41.7128,
+          longitude: -75.0059
+        },
+        title: "Hello 2"
+      },
+      {
+        id: 2,
+        coords: {
+          latitude: 42.7128,
+          longitude: -76.0059
+        },
+        title: "Hello 3"
+      }
+    ]
+    $scope.onClick = function(marker, eventName, model) {
+      model.show = !model.show;
+    }
 		$scope.update = function(epic) {
 			$scope.epic = epic;
 			console.log($scope.epic)
 			$scope.epic.$update({id: epic.id})
 		}
-
 		$scope.delete = function(epic) {
 			$scope.epic = epic;
 			$scope.epic.$delete({id: epic.id})
