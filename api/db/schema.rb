5d070bef43e5b0330102be82be574c230a4b3848
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-08-23 10:54:55 -0400

    added date to story model

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160822200509) do
+ActiveRecord::Schema.define(version: 20160823143342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160822200509) do
     t.integer  "epic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date     "date"
     t.index ["epic_id"], name: "index_stories_on_epic_id", using: :btree
   end
 
--- api/db/seeds.rb
+++ api/db/seeds.rb
@@ -12,7 +12,7 @@
 scotland = Epic.create(title: "My Awesome Scotland Trip!", name: "Snorlax McDuff", summary: "It was awesome!", img_url: "http://www.studyacrossthepond.com/sites/default/files/styles/panopoly_image_featured/public/scotland.jpg?itok=bJet87Ob", lat: 56.490671, long: -4.202646)
 africa = Epic.create(title: "African Safari!", name: "Jorp Jorpson", summary: "I bless the rainnssss down in Affricaaaa", img_url: "http://www.fodors.com/wire/African-Safari-Elephants-Kilimanjaro.jpg", lat: -8.783195, long: 34.508523)
 
-Story.create(title: "Went to awesome castle", map_loc: "www.google.com", media_url: "http://www.goruralscotland.com/image_uploading/blueimp/server/php/files/507da6148057c.jpg", summary: "It was so awesome!", epic: scotland)
-Story.create(title: "Went to awesome pub", map_loc: "www.google.com", media_url: "http://www.scottishpub.ro/fundal/scottish-pub2.jpg", summary: "Drank much beer.", epic: scotland)
-Story.create(title: "Elephants!", map_loc: "www.google.com", media_url: "http://assets.worldwildlife.org/photos/885/images/carousel_small/African_Elephant_7.27.2012_whytheymatter_HI_58709.jpg?1345580947", summary: "ELEPHANTS!", epic: africa)
-Story.create(title: "Food!", map_loc: "www.google.com", media_url: "http://www.africanfoods.co.uk/images/rice-and-chicken-the-African-way.jpg", summary: "Food so yummy!", epic: africa)
+Story.create(title: "Went to awesome castle", map_loc: "www.google.com", media_url: "http://www.goruralscotland.com/image_uploading/blueimp/server/php/files/507da6148057c.jpg", summary: "It was so awesome!", date: DateTime.new(2016, 1, 15), epic: scotland)
+Story.create(title: "Went to awesome pub", map_loc: "www.google.com", media_url: "http://www.scottishpub.ro/fundal/scottish-pub2.jpg", summary: "Drank much beer.", date: DateTime.new(2016, 1, 9), epic: scotland)
+Story.create(title: "Elephants!", map_loc: "www.google.com", media_url: "http://assets.worldwildlife.org/photos/885/images/carousel_small/African_Elephant_7.27.2012_whytheymatter_HI_58709.jpg?1345580947", summary: "ELEPHANTS!", date: DateTime.new(2015, 10, 22), epic: africa)
+Story.create(title: "Food!", map_loc: "www.google.com", media_url: "http://www.africanfoods.co.uk/images/rice-and-chicken-the-African-way.jpg", summary: "Food so yummy!", date: DateTime.new(2015, 10, 26), epic: africa)
