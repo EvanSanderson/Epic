d9e033d99a13c7af53501d57baee09a2724148fb
Van Phan <VanPhan514@gmail.com>
2016-08-25 09:22:53 -0400

    Fix hrefs

--- js/home/index.controller.js
+++ js/home/index.controller.js
@@ -76,7 +76,7 @@
 
       // Marker events \\
       $scope.markerClick = function(marker){
-        var contentString = '<a class="window_link window_wrapper" href="/Epic/#/epic/"'+marker.model.id+'><h3 class="window_header">'+marker.model.title+'</h3>' + '<img class="window_img" src='+marker.model.img+'>' + '<p>'+marker.model.sum+'</p></a>'
+        var contentString = '<a class="window_link window_wrapper" href="/Epic/#/epic/' +marker.model.id+ '"><h3 class="window_header">'+marker.model.title+'</h3>' + '<img class="window_img" src='+marker.model.img+'>' + '<p>'+marker.model.sum+'</p></a>'
           var infowindow = new google.maps.InfoWindow({
             content: contentString,
             scrollwheel: false
