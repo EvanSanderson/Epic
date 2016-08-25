f8777789f10c19dae242b36a5fed03c68d81716e
Van Phan <VanPhan514@gmail.com>
2016-08-25 09:37:02 -0400

    Fixed URL

--- js/home/index.controller.js
+++ js/home/index.controller.js
@@ -26,7 +26,7 @@
           var item = this.epic
           this.epic.$save().then(function(){
             var newEpic = document.createElement('a');
-            newEpic.setAttribute('href', '/Epic/#/epics/' + item.id);
+            newEpic.setAttribute('href', '/Epic/#/epic/' + item.id);
             angular.element(newEpic).append(item.title);
             angular.element(document.getElementById("epic-list")).append(newEpic);
             $state.transitionTo('epicIndex', null, {reload: true});
@@ -77,7 +77,7 @@
       // Marker events \\
       $scope.markerClick = function(marker){
         console.log(marker);
-        var contentString = "<a class='window_link window_wrapper' href='/Epic/#/epics/" + marker.model.id + "'><h3 class='window_header'>" + marker.model.title + "</h3>" + "<img class='window_img' src='" + marker.model.img + "'>" + "<p>" + marker.model.sum + "</p></a>";
+        var contentString = "<a class='window_link window_wrapper' href='/Epic/#/epic/" + marker.model.id + "'><h3 class='window_header'>" + marker.model.title + "</h3>" + "<img class='window_img' src='" + marker.model.img + "'>" + "<p>" + marker.model.sum + "</p></a>";
         console.log(contentString)
           var infowindow = new google.maps.InfoWindow({
             content: contentString,
