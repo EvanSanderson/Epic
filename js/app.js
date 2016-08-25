1d03e8a0ff2ecad8d1a48077438b01035e476bb2
Van Phan <VanPhan514@gmail.com>
2016-08-25 10:04:51 -0400

    Got changes from loc branch

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@
     ])
     .controller('MainController', function(HomeFactory, $scope, $state) {
         $scope.$state = $state;
-        console.log($scope.$state)
+        console.log(fqn($scope.$state))
         console.log($scope.$state.includes('epicIndex'))
     	  $scope.epics = HomeFactory.query();
     	  $scope.open = function() {
